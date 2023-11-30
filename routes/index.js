2332baed583b55594e849449f9c414cb0b8541c7
rnjsdbswo00 <72205683+rnjsdbswo00@users.noreply.github.com>
2023-12-01 01:48:39 +0900

    23_12_01_v1

--- routes/index.js
+++ routes/index.js
@@ -4,5 +4,12 @@ module.exports = function(passport){
         res.render('main.html')
     });
 
+    router.get('/login', function(req, res, next) {
+        res.render('login.html')
+    });
+    router.get('/register', function(req, res, next) {
+        res.render('register.html')
+    });
+    
     return router;
 }
